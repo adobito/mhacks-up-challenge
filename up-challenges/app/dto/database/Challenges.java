01212a61d772e9b86cfac95caa9c221fdf0ebc45
Jesus Luzon <jesus.e.luzon@gmail.com>
2014-09-07 02:35:20 -0400

    Added generation types.

--- up-challenges/app/dto/database/Challenges.java
+++ up-challenges/app/dto/database/Challenges.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -29,7 +30,7 @@ public class Challenges {
 	}
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ID")
 	public Integer getId() {
 		return id;
--- up-challenges/app/dto/database/User.java
+++ up-challenges/app/dto/database/User.java
@@ -3,6 +3,7 @@ package dto.database;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -20,7 +21,7 @@ public class User {
 		super();
 	}
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ID")
 	public Integer getId() {
 		return id;
